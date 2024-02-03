ae2640009b7f4662a66d8b557e04b24601733c09
PranavKuramkoteSudhir <139109363+PranavKuramkoteSudhir@users.noreply.github.com>
2024-02-03 11:47:59 -0500

    Update consumer2.py

--- consumer2.py
+++ consumer2.py
@@ -1,27 +1,25 @@
-import streamlit as st
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
 import json
 import pandas as pd
-import matplotlib.pyplot as plt
+from sentiment_anlysis import sentiment_analysis_with_rating
 
-st.title("Live Updates from Kafka")
-
+# Define Kafka consumer and producer
 consumer = KafkaConsumer('order_detail', bootstrap_servers='localhost:29092', value_deserializer=json.loads)
+producer = KafkaProducer(bootstrap_servers="localhost:29092")
 
+# Define DataFrame columns
 columns = ['review_id', 'user_id', 'business_id', 'stars', 'useful', 'funny', 'cool', 'text', 'date', 'category']
 df = pd.DataFrame(columns=columns)
-plot_container = st.empty()
-table_container = st.empty()
 
-
+# Continuously process messages
 for message in consumer:
     value = message.value
     new_row = pd.DataFrame([value])
     df = pd.concat([df, new_row], ignore_index=True)
 
-    # Group data based on 'category' and calculate the average stars
-    avg_stars_by_category = df.groupby('category')['stars'].mean()
+    # Perform sentiment analysis on the latest 'text' column
+    df['user_sentiment_rating'] = sentiment_analysis_with_rating(df['text'])
 
-    # Display the table and plot
-    table_container.table(df)
-    plot_container.bar_chart(avg_stars_by_category)
+    # Send sentiment analysis results to Kafka topic
+    producer.send('sentiment_analysis', json.dumps(df.to_dict(orient='records')).encode("utf-8"))
